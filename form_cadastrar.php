cd315ae785bc8c50b7fc179fee3880b71477d902
lauraaromano <161627881+lauraaromano@users.noreply.github.com>
2025-08-22 18:24:38 -0300

    conectar com banco de dados

--- form_cadastrar.php
+++ form_cadastrar.php
@@ -6,15 +6,15 @@
     <div class="container">
         <h1>Bem vindo ao 1º Sistema Web com CRUD</h1>
         <h2>Laura Romano e Evellyn Furtado</h2>
-        <form>
+        <form action="inserir.php" method="POST">
             <div class="mb-3">
                 <input type="text" class="form-control" name="produto" placeholder="Digite o nome do produto">
             </div>
             <div class="mb-3">
-                <input type="text" class="form-control" placeholder="Digite o preço do produto">
+                <input type="text" class="form-control" name="preco" placeholder="Digite o preço do produto">
             </div>
             <div class="mb-3">
-                <input type="text" class="form-control"  placeholder="Digite a quantidade do produto">
+                <input type="text" class="form-control" name="quantidade" placeholder="Digite a quantidade do produto">
             </div>
             <button type="submit" class="btn btn-primary">Cadastrar</button>
             <a href="index.php" type="button" class="btn btn-warning">Voltar</a>
--- listar.php
+++ listar.php
@@ -15,33 +15,32 @@
                         <th scope="col">PREÇO</th>
                         <th scope="col">QUANTIDADE</th>
                         <th scope="col">OPÇÕES</th>
+                        
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Teclado Gamer</td>
-                        <td>120,99</td>
-                        <td>10</td>
-                        <td>
-                            <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                                <a href="#" type="button" class="btn btn-danger">Atualizar</a>
-                                <a href="#" type="button" class="btn btn-warning">Apagar</a>
+
+                <?php
+                        require 'conexao.php';
+                        $sql = "SELECT * FROM produtos";
+                        $stmt = $pdo->query($sql);
+                        while ($produto = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                            echo"<tr>";
+                            echo"<td>".$produto['id']."</td>";
+                            echo"<td>".$produto['nome']."</td>";
+                            echo"<td>".$produto['preco']."</td>";
+                            echo"<td>".$produto['quantidade']."</td>";
+                            echo"
+                            <td>
+                                <div class='btn-group' role='group' aria-label='Basic mixed styles example'>
+                                <a href='#' type='button' class='btn btn-danger'>Atualizar</a>
+                                <a href='#' type='button' class='btn btn-warning'>Apagar</a>
                             </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Monitor LCD 20 polegadas</td>
-                        <td>599,99</td>
-                        <td>70</td>
-                        <td>
-                            <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                                <a href="#" type="button" class="btn btn-danger">Atualizar</a>
-                                <a href="#" type="button" class="btn btn-warning">Apagar</a>
-                            </div>
-                        </td>
-                    </tr>
+                            </td>
+                            ";
+                            echo"</tr>";
+                        }
+                        ?>
                 </tbody>
             </table>
             <a href="index.php" type="button" class="btn btn-warning">Voltar</a>
